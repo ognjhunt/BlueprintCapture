dce65c8752a8af8f7394eedbcbbff593bda8e820
ognjhunt <ohstnhunt@gmail.com>
2025-10-21 11:17:00 -0400

    Initial Commit

---
+++ BlueprintCapture/BlueprintCapture-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  BlueprintCapture-Bridging-Header.h
+//  BlueprintCapture
+//
+//  Created by Nijel A. Hunt on 10/21/25.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
---
+++ BlueprintCaptureTests/BlueprintCaptureTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlueprintCaptureTests.swift
+//  BlueprintCaptureTests
+//
+//  Created by Nijel A. Hunt on 10/21/25.
+//
+
+import Testing
+@testable import BlueprintCapture
+
+struct BlueprintCaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
